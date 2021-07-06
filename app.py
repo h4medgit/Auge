b5db86fe0ad3376089e29eb24fc45b44e658c4ea
Hamed Mohammadvand <70788776+hamedmvd@users.noreply.github.com>
2021-07-06 07:56:35 +0430

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+# choosing foot or inch and input your number to foot:
+foot = int(input('input foot number: '))
+
+#inch
+print("inch is:", foot * 12)
+
+#m 
+metr = foot * 0.3048
+print("m is:", metr)
+
+#cm
+cm = metr*100
+print("cm is:", cm)
